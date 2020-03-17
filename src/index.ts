2115e22fbd9b468efe4f26e4219615b259239b21
lynnkoo <lynnkoo.1120@gmail.com>
2020-03-17 18:22:47 +0800

    update version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-export function normalizeArray(value: any) {
-    return [].concat(value).filter(Boolean)
+
+export function normalizeArray<T>(value?: T | T[]): T[] {
+  const empty = [] as T[]
+  if (!value) {
+    return empty
   }
+  return empty.concat(value).filter(Boolean)
+}
 
 export function fullTypeOf(value: any) {
     return Object.prototype.toString.call(value).slice(8, -1).toLocaleLowerCase()
